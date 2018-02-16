5087ee7d6f26f454d7d73fe5dab1b4d78290ce88
Artur Kaluza <akaluza26@gmail.com>
2018-02-16 10:46:22 +0100

    Added Routes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import { getCountries } from './actions/actions-countries';
+import routes from './routes';
 
 store.dispatch(getCountries());
 
 render(
     <Provider store={store}>
       <div>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes}/>
         <DevTools />
       </div>
     </Provider>,
